108bf9cc9229d6d853b304ec2a9de023377501cd
Lokesh7913 <lokeshchand7913@gmail.com>
2023-12-01 18:16:05 +0530

    first commit

---
+++ src/main/java/controller/DocManagement.java
@@ -0,0 +1,16 @@
+package controller;
+
+public class DocManagement {
+	private String fullName;
+	private String id;
+	private String emailID;
+	private String designation;
+	private String qualification;
+	private String workLocation;
+	private long contactNumber;
+	
+	public static void main(String... args) {
+		System.out.println("first docManagement controller");
+		
+	}
+}
